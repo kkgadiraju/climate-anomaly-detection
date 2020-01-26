b39091639dc9de4a8b5d9a1cb88be513601e6cf9
Krishna Karthik Gadiraju <kkgadiraju@gmail.com>
2020-01-26 15:23:12 -0500

    add data folder

---
+++ data/ncep2data.py
@@ -0,0 +1,15 @@
+####
+## Download daily avg surface temperatures 
+## Data will be in netcdf format
+## Reference: https://stackoverflow.com/questions/11768214/python-download-a-file-over-an-ftp-server/12424311
+###
+
+import shutil
+import urllib.request as request
+from contextlib import closing
+for i in range(1979,1990):
+    path = "ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis2.dailyavgs/gaussian_grid/air.2m.gauss."+str(i)+".nc"
+    print(f'Downloading {path}')
+    with closing(request.urlopen(path)) as r:
+        with open("./air.2m.gauss."+str(i)+".nc", 'wb') as f:
+            shutil.copyfileobj(r, f)
